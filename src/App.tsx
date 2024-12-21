43b1e3a4f3ef166919a4f788ac17f628a1ad4dc8
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 16:03:58 -0500

    Changed text. Should updated on EC2 if everything was done correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <h1 className="bg-zinc-900 text-white h-screen w-screen flex justify-center items-center text-xl">
-      Hello World <br /> Nice
+      Welcome to this beautiful site
     </h1>
   )
 }

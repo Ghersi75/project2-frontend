440aec5022955ee11a5ffc29a0ac0530b650fb13
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 18:41:15 -0500

    Created basic Login page

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,7 +1,38 @@
+import { Button } from "../ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
 export default function Login() {
-  return(
-    <>
-      Nice
-    </>
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle> Login </CardTitle>
+        <CardDescription> Enter your email and password to log in to your account </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email"> Email </Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="example@email.com"
+                required />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
   )
 }

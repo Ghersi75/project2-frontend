62b4f469f9e988c65479182b2005f155c09e8299
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 11:10:49 -0500

    Created react project with Vite and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="bg-zinc-900 text-white h-screen w-screen flex justify-center items-center text-xl">
+      Hello World
+    </h1>
+  )
+}
+
+export default App

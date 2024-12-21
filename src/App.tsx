554580dd09c4c46fa44ca18c339280b7ead6c76e
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 18:53:19 -0500

    Added AuthLayout component for styling only Login/SignUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import Login from "@/Components/Login/Login";
 import { ThemeProvider } from "./Contexts/ThemeProvider";
 import SignUp from "@/Components/SignUp/SignUp";
 import { BrowserRouter, Route, Routes } from "react-router";
+import AuthLayout from "./Components/AuthLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <h1 className="bg-muted text-white h-screen w-screen flex justify-center items-center text-xl">
-          <Routes>
+        <Routes>
+          <Route element={<AuthLayout />}>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-          </Routes>
-        </h1>
+          </Route>
+        </Routes>
       </ThemeProvider>
     </BrowserRouter>
   )

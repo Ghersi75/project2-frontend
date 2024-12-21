6d58081b0344593064aca55516820f66df33d286
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 18:18:34 -0500

    Added path resolve so that the absolute path can actually be resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // Updated config according to guide above in order to properly expose the application while running in a docker container
 export default defineConfig({
@@ -15,4 +16,10 @@ export default defineConfig({
     host: true,
     origin: "http://0.0.0.0:8080",
   },
+  resolve: {
+    alias: {
+      // https://www.geeksforgeeks.org/how-to-create-absolute-imports-in-vite-react-app/
+      "@": resolve(__dirname, "src")
+    }
+  }
 });

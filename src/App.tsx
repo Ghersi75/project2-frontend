b995e1ab3ce4ae77cd5cf0a3d38c24b9a626a9bc
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 18:15:11 -0500

    Created login component and imported into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Login from "@/Components/Login/Login";
+
 function App() {
   return (
     <h1 className="bg-zinc-900 text-white h-screen w-screen flex justify-center items-center text-xl">
       Welcome to this beautiful site
+      <Login />
     </h1>
   )
 }

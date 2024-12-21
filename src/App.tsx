c7d43a8a154d0db853a93a117e1ac1d4b167ee09
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 15:40:56 -0500

    Added Nice for testing that image creation works as expected

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <h1 className="bg-zinc-900 text-white h-screen w-screen flex justify-center items-center text-xl">
-      Hello World
+      Hello World <br /> Nice
     </h1>
   )
 }

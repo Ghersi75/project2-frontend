308c1e272c61cc3ff15831c5b52c3b82647cdc2b
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-21 18:45:38 -0500

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import Login from "@/Components/Login/Login";
 import { ThemeProvider } from "./Contexts/ThemeProvider";
 import SignUp from "@/Components/SignUp/SignUp";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <h1 className="bg-muted text-white h-screen w-screen flex justify-center items-center text-xl">
-        <SignUp />
-      </h1>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1 className="bg-muted text-white h-screen w-screen flex justify-center items-center text-xl">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </h1>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 

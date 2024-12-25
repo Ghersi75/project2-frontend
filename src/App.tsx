fc0ae59acb0bfed05084b7a0f6138d3d27265c89
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-25 17:39:52 -0500

    Updated SignUp to SignUpController and added route with parameter to load a page based on a game's appId

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Login from "@/Components/Login/Login";
 import { ThemeProvider } from "./Contexts/ThemeProvider";
-import SignUp from "@/Components/SignUp/SignUp";
 import { BrowserRouter, Route, Routes } from "react-router";
 import AuthLayout from "./Components/AuthLayout";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+import GamePage from "./Components/GamePage/GamePage";
+import SignUpController from "./Components/SignUp/SignUpController";
 
 function App() {
   console.log(import.meta.env.VITE_BACKEND);
@@ -16,9 +17,10 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="" element={<Home />} />
+            <Route path="/:appId" element={<GamePage />}/>
             <Route element={<AuthLayout />}>
               <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signup" element={<SignUpController />} />
             </Route>
           </Routes>
         </div>

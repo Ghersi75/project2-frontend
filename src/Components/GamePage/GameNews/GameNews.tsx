e3df75a631b3147357f5f583473720d9bffc78fe
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-30 16:22:03 -0500

    Created dedicated GameNews component to handle fetching and displaying the data

---
+++ src/Components/GamePage/GameNews/GameNews.tsx
@@ -0,0 +1,47 @@
+import { Label } from "@/Components/ui/label";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import GameNewsCard from "../GameNewsCard";
+import { useParams } from "react-router";
+import { Card, CardHeader, CardTitle } from "@/Components/ui/card";
+
+export default function GameNews() {
+  const [news, setNews] = useState<any>([]);
+  const { appId } = useParams();
+
+  useEffect(() => {
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${appId}`)
+      .then(res => {
+        console.log(res.data.appnews.newsitems)
+        setNews(res.data.appnews.newsitems)
+      })
+      .then(err => {
+        console.error(err)
+      })
+  }, []);
+
+  if (news.length == 0) {
+    return (
+      <Card className="w-fit">
+        <CardHeader>
+          <CardTitle>
+            <Label> No News Found </Label>
+          </CardTitle>
+        </CardHeader>
+      </Card>
+    )
+  }
+
+  return (
+    <>
+      {
+        news.map((item, idx) => {
+          return (
+            <GameNewsCard item={item} key={idx} />
+          )
+        })
+      }
+    </>
+
+  )
+}
--- src/Components/GamePage/GamePageSidebar.tsx
+++ src/Components/GamePage/GamePageSidebar.tsx
@@ -1,28 +1,15 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useParams } from "react-router";
-import GameNewsCard from "./GameNewsCard";
 import { Button } from "../ui/button";
+import GameNews from "./GameNews/GameNews";
 
 export default function GamePageSidebar() {
-  const [news, setNews] = useState<any>([]);
   const [pageSelected, setPageSelected] = useState<"news" | "threads">("news");
   const { appId } = useParams();
 
   if (appId == undefined) {
     return;
   }
-
-  useEffect(() => {
-    axios.get(`https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${appId}`)
-      .then(res => {
-        console.log(res.data.appnews.newsitems)
-        setNews(res.data.appnews.newsitems)
-      })
-      .then(err => {
-        console.error(err)
-      })
-  }, []);
 
   const handlePageSelectedClick = (page: "news" | "threads") => {
     if (page == pageSelected) {
@@ -38,11 +25,7 @@ export default function GamePageSidebar() {
         <Button variant={pageSelected == "news" ? "outline" : "ghost"} onClick={() => { handlePageSelectedClick("news") }}> News </Button>
         <Button variant={pageSelected == "threads" ? "outline" : "ghost"} onClick={() => { handlePageSelectedClick("threads") }} > Threads </Button>
       </div>
-      {pageSelected == "news" && news.map((item, idx) => {
-        return (
-          <GameNewsCard item={item} key={idx} />
-        )
-      })}
+      {pageSelected == "news" && <GameNews />}
       {pageSelected == "threads" &&
         <>
           Threads

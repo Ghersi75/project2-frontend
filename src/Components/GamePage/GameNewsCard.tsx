c7f693c7a1eefe568c373349d78bbb2c04adb688
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-30 15:29:10 -0500

    Installed bulletin board parser to attempt to parse steam community news articles. Works well enough, but is not perfect

--- src/Components/GamePage/GameNewsCard.tsx
+++ src/Components/GamePage/GameNewsCard.tsx
@@ -2,9 +2,13 @@ import { Button } from "@/Components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/Components/ui/card";
 import { useState } from "react";
 import { Link } from "react-router";
+// https://github.com/tada5hi/bulletin-board-code
+// Works well enough for this use case
+import { Parser } from 'bulletin-board-code';
 
 export default function GameNewsCard({ item }: any) {
   const [showMore, setShowMore] = useState(false);
+  const parser = new Parser();
 
   return (
     <Card>
@@ -19,7 +23,7 @@ export default function GameNewsCard({ item }: any) {
       <CardContent className="grid gap-2">
         {/* TODO: Find a better way if there's time */}
         {/* Not smart to do, but since this is returned from the steam api we'll trust it */}
-        <div dangerouslySetInnerHTML={{ __html: item.contents }} className={`overflow-hidden ${showMore ? "" : "line-clamp-3"}`} />
+        <div dangerouslySetInnerHTML={ item.feedlabel == "Community Announcements" ? {__html: parser.fromBBCode(item.contents)} : { __html: item.contents }} className={`overflow-hidden ${showMore ? "" : "line-clamp-3"}`} />
         <Button variant="link" className="text-blue-500 justify-self-end" onClick={() => { setShowMore(prev => !prev) }}> {showMore ? "Show less" : "Show more"} </Button>
       </CardContent>
       <CardFooter className="">

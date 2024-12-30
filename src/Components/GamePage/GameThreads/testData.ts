8970faaa44248a49429582e23f8aad47f8ed4851
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-30 18:05:08 -0500

    Made some test data for threads/reviews

---
+++ src/Components/GamePage/GameThreads/testData.ts
@@ -0,0 +1,16 @@
+export const testData = [
+  {
+    username: "ghersi",
+    displayName: "Ghersi75",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliter enim explicari, quod quaeritur, non potest. Negare non possum. Quis istud, quaeso, nesciebat? Summum ením bonum exposuit vacuitatem doloris; Fortemne possumus dicere eundem illum Torquatum? Duo Reges: constructio interrete.",
+    likes: 34,
+    dislikes: 3
+  },
+  {
+    username: "testuser",
+    displayName: "Just some user",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tamen a proposito, inquam, aberramus. Neminem videbis ita laudatum, ut artifex callidus comparandarum voluptatum diceretur. Nunc dicam de voluptate, nihil scilicet novi, ea tamen, quae te ipsum probaturum esse confidam. Intrandum est igitur in rerum naturam et penitus quid ea postulet pervidendum; Si sapiens, ne tum quidem miser, cum ab Oroete, praetore Darei, in crucem actus est. Heri, inquam, ludis commissis ex urbe profectus veni ad vesperum. Sapientem locupletat ipsa natura, cuius divitias Epicurus parabiles esse docuit. An dolor longissimus quisque miserrimus, voluptatem non optabiliorem diuturnitas facit? Duo Reges: constructio interrete. Quibus autem in rebus tanta obscuratio non fit, fieri tamen potest, ut id ipsum, quod interest, non sit magnum.",
+    likes: 45,
+    dislikes: 99
+  },
+]

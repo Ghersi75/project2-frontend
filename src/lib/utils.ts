370b46d6257e5a130008af6e3e477e914e79288b
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-30 16:24:09 -0500

    Created basic format time function to format steam news date to a user friendly format

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// https://www.geeksforgeeks.org/how-to-format-date-in-typescript/
+// Steam api seems to return unix time in seconds
+export function formatTime(unix: number): string {
+  const locale = 'en-US'
+  const options: Intl.DateTimeFormatOptions = {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  }
+  const formatter = Intl.DateTimeFormat(locale, options)
+  const formattedDate = formatter.format(unix * 1000)
+
+  return formattedDate
+}

b0baf5d84b5ec969b2af4a915cc788f189392cf0
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-30 16:21:32 -0500

    Created hook to check if description is truncated. Basically used to check if to show the "show more/less" button or not

--- src/Components/GamePage/GameNewsCard.tsx
+++ src/Components/GamePage/GameNewsCard.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/Components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/Components/ui/card";
-import { useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 // https://github.com/tada5hi/bulletin-board-code
 // Works well enough for this use case
 import { Parser } from 'bulletin-board-code';
+import { Label } from "../ui/label";
+import { formatTime } from "@/lib/utils";
+import { useTruncatedElement } from "@/Hooks/useTruncatedElement";
 
 export default function GameNewsCard({ item }: any) {
-  const [showMore, setShowMore] = useState(false);
+  const descriptionRef = useRef(null);
+  const { isTruncated, isShowingMore, toggleIsShowingMore } = useTruncatedElement({ ref: descriptionRef });
   const parser = new Parser();
 
   return (
@@ -16,15 +20,18 @@ export default function GameNewsCard({ item }: any) {
         <CardTitle>
           {item.title}
         </CardTitle>
-        <CardDescription>
-          {item.feedlabel}
+        <CardDescription className="flex justify-between">
+          <Label> {item.feedlabel} </Label>
+          <Label> {formatTime(item.date)} </Label>
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-2">
         {/* TODO: Find a better way if there's time */}
         {/* Not smart to do, but since this is returned from the steam api we'll trust it */}
-        <div dangerouslySetInnerHTML={ item.feedlabel == "Community Announcements" ? {__html: parser.fromBBCode(item.contents)} : { __html: item.contents }} className={`overflow-hidden ${showMore ? "" : "line-clamp-3"}`} />
-        <Button variant="link" className="text-blue-500 justify-self-end" onClick={() => { setShowMore(prev => !prev) }}> {showMore ? "Show less" : "Show more"} </Button>
+        <div ref={descriptionRef} dangerouslySetInnerHTML={item.feedlabel == "Community Announcements" ? { __html: parser.fromBBCode(item.contents) } : { __html: item.contents }} className={`overflow-hidden ${isShowingMore ? "max-h-[500px] overflow-y-scroll" : "line-clamp-3 overflow-y-hidden"}`} />
+        {isTruncated &&
+          <Button variant="link" className="text-blue-500 justify-self-end" onClick={toggleIsShowingMore}> {isShowingMore ? "Show less" : "Show more"} </Button>
+        }
       </CardContent>
       <CardFooter className="">
         <Link to={item.url}>

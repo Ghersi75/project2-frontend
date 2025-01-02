38d65c931d8d524825996b799f15eb2834f48161
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-02 12:47:09 -0500

    Added axios and testing that api call works on ec2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import axios from "axios"
+import { useEffect } from "react"
+
 function App() {
+  useEffect(() => {
+    axios.get("https://store.steampowered.com/api/featuredcategories/", {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
+    .then(res => console.log(res));
+  }, [])
+
   return (
     <h1 className="bg-zinc-900 text-white h-screen w-screen flex justify-center items-center text-xl">
       Welcome to this somewhat beautiful site

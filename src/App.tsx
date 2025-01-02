42cd92cc736cae958736c2316f434d0699527bc2
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-02 13:08:21 -0500

    Testing nginx reverse proxy to make sure request works

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 
 function App() {
   useEffect(() => {
-    axios.get("https://store.steampowered.com/api/featuredcategories/", {
+    axios.get("http://ec2-3-137-66-132.us-east-2.compute.amazonaws.com/steam-api/", {
       headers: {
         "Access-Control-Allow-Origin": "*"
       }

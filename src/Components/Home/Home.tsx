e0a46e45138405ea7088c28a2c526acfc489b15a
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-02 13:50:49 -0500

    Removed comments and printing api result

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -8,8 +8,6 @@ export default function Home() {
   const [topSellers, setTopSellers] = useState<HomeGameInfoType[]>([]);
   const [newReleases, setNewReleases] = useState<HomeGameInfoType[]>([]);
   const [comingSoon, setComingSoon] = useState<HomeGameInfoType[]>([]);
-  // const [topSellers, setSpecials] = useState<GameInfoType[]>([]);
-  // const [specials, setSpecials] = useState<GameInfoType[]>([]);
 
   useEffect(() => {
     axios.get(import.meta.env.VITE_STEAM_FEATURED)
@@ -18,69 +16,14 @@ export default function Home() {
         setTopSellers(res.data.top_sellers.items);
         setNewReleases(res.data.new_releases.items);
         setComingSoon(res.data.coming_soon.items);
-        console.log(res)
       })
       .catch(err => {
         console.log(err)
       })
   }, []);
 
-  // useEffect(() => {
-  //   if (search == "") {
-  //     return;
-  //   }
-
-  //   const timer = setTimeout(async () => {
-  //     axios.get(`https://cors-anywhere.herokuapp.com/https://steamcommunity.com/actions/SearchApps/${search}`, {
-  //       headers: {
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  //       }
-  //     })
-  //       .then(res => {
-  //         console.log(res.data)
-  //         setSearchResults(res.data)
-  //       })
-  //       .catch(err => console.error(err));
-  //   }, 1000)
-
-  //   return () => {
-  //     clearTimeout(timer)
-  //   }
-  // }, [search])
-
-  // const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearch(e.target.value);
-  // }
-
-  // const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-  //   if (e.key === "Enter") {
-  //     // console.log("Enter");
-  //     axios.get(`https://cors-anywhere.herokuapp.com/https://steamcommunity.com/actions/SearchApps/${search}`, {
-  //       headers: {
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  //       }
-  //     })
-  //       .then(res => {
-  //         console.log(res.data)
-  //         setSearchResults(res.data)
-  //       })
-  //       .catch(err => console.error(err));
-  //   }
-  // }
-
   return (
     <div className="grow p-8 flex flex-col gap-4">
-      {/* <Input className="w-3/4" type="text" value={search} onChange={(e) => { setSearch(e.target.value) }} onKeyDown={handleKeyPress} />
-      {
-        searchResults.map((item, idx) => {
-          return (
-            // https://stackoverflow.com/a/66180668
-            <div key={idx}> {item.name}, {item.appId} <img src={item.logo} /> <img src={item.icon} /></div>
-          )
-        })
-      } */}
       <h1 className="text-3xl"> Current Specials </h1>
       <div className="grid gap-8 2xl:grid-cols-3 grid-cols-2 w-full justify-end">
         {

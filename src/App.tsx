d579db03259eada24797a7b45005033229703614
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-02 12:41:25 -0500

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <h1 className="bg-zinc-900 text-white h-screen w-screen flex justify-center items-center text-xl">
-      Welcome to this beautiful site
+      Welcome to this somewhat beautiful site
     </h1>
   )
 }

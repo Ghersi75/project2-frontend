5b9552210605cbc3ccdd34468f67c537cdfeb3e6
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-02 14:19:12 -0500

    Brought over theme provider from features branch for light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import SignUpController from "./Components/SignUp/SignUpController";
 import LoginController from "./Components/Login/LoginController";
 import AuthLayout from "./Components/AuthLayout";
+import { ThemeProvider } from "./Contexts/ThemeProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="/login" element={<LoginController />} />
-          <Route path="/signup" element={<SignUpController />} />
-        </Route>
-      </Routes>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Routes>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<LoginController />} />
+            <Route path="/signup" element={<SignUpController />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   )
 }

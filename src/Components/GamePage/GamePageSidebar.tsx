3392f374774c414608b8b6e2a99ec760292450f7
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-07 15:15:08 -0500

    Updated method to keep track of whether user is on threads section or news section so it persists when page is refreshed

--- src/Components/GamePage/GamePageSidebar.tsx
+++ src/Components/GamePage/GamePageSidebar.tsx
@@ -7,7 +7,7 @@ import useNewsFeedShown from "@/Hooks/useNewsFeedShown";
 
 export default function GamePageSidebar() {
   const { appId } = useParams();
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const viewThreads = searchParams.get("viewThreads");
   // Render threads if search parameter is present
   const [pageSelected, setPageSelected] = useState<"news" | "threads">(viewThreads != null ? "threads" : "news");
@@ -26,11 +26,24 @@ export default function GamePageSidebar() {
       return;
     }
 
+    if (page == "news") {
+      if (viewThreads != null) {
+        setSearchParams();
+      }
+    }
+
+    if (page == "threads") {
+      if (viewThreads == null) {
+        setSearchParams({
+          viewThreads: ""
+        })
+      }
+    }
     setPageSelected(page);
   }
 
   return (
-    <div className="w-[400px] max-h-svh bg-secondary/20 justify-self-end bg-opacity-10 p-4 flex flex-col overflow-scroll gap-4">
+    <div className="w-[400px] max-h-svh bg-secondary/20 justify-self-end bg-opacity-10 p-4 flex flex-col overflow-y-scroll overflow-x-hidden gap-4">
       <div className="flex flex-row gap-4">
         <Button variant={pageSelected == "news" ? "outline" : "ghost"} onClick={() => { handlePageSelectedClick("news") }}> News </Button>
         <Button variant={pageSelected == "threads" ? "outline" : "ghost"} onClick={() => { handlePageSelectedClick("threads") }} > Threads </Button>

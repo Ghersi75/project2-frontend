75936238d5b6bd5542297166774dfe0f8709753f
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-09 17:58:25 -0500

    Updated styling of navbar. Added GameThreads, profile page link and moved username/displayName and logout to the bottom of navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -14,33 +14,54 @@ export default function Navbar() {
   const urlAdditions = `?${appId != null ? `appId=${appId}` : ""}&${paramsString}`
 
   return (
-    <nav className="w-[300px] bg-secondary sticky top-0 h-svh p-8">
-      <div className="w-full flex flex-col gap-4">
-        <Link to="" className={cn(linkStyle, "text-2xl")}> Home </Link>
-        {/* Profile or login/sign up */}
-        {
-          userInfo == null &&
+    <nav className="w-[300px] bg-secondary sticky top-0 h-svh p-8 flex flex-col justify-between">
+      <div>
+        <Link to="/">
+          <Button variant="ghost" className="text-3xl mb-8 p-0">
+            Game Threads
+          </Button>
+        </Link>
+        <div className="w-full flex flex-col gap-4">
+          <Link to="" className={cn(linkStyle, "text-2xl")}> Home </Link>
+          {/* Profile or login/sign up */}
+          <Link to="/search" className={linkStyle}> Search </Link>
+
+
+          {/* Theese 2 are only available if logged in */}
+          {
+            userInfo != null &&
+            <>
+              <Link to="/favorites">
+                <h1 className="hover:underline hover:cursor-pointer"> Favorited Games </h1>
+              </Link>
+              <h1 className="hover:underline hover:cursor-pointer" onClick={() => { setNewsFeedShown(prev => !prev) }}> {newsFeedShown == true ? "Hide News Feed" : "Show News Feed"} </h1>
+            </>
+          }
+        </div>
+      </div>
+      {
+        userInfo ?
+          <div className="flex flex-row justify-between items-center">
+            <Link to="/profile">
+              <div className="flex flex-col gap-1">
+                <h1 className="text-2xl">
+                  {userInfo.displayName}
+                </h1>
+                <h2 className="text-muted-foreground text-sm">
+                  @{userInfo.username}
+                </h2>
+              </div>
+            </Link>
+            <Button className="hover:underline w-fit text-muted-foreground" onClick={logout}>
+              logout
+            </Button>
+          </div>
+          :
           <div className="flex gap-8">
             <Link to={`/login${urlAdditions}`} className={linkStyle}> Login </Link>
             <Link to={`/signup${urlAdditions}`} className={linkStyle}> Sign Up </Link>
           </div>
-        }
-        <h1> {userInfo ? `${userInfo.username} ${userInfo.displayName}` : "Profile"} </h1>
-        {userInfo && <Button variant="ghost" className="hover:underline w-fit bg-red-500" onClick={logout}> logout </Button>}
-        <Link to="/search" className={linkStyle}> Search </Link>
-
-
-        {/* Theese 2 are only available if logged in */}
-        {
-          userInfo != null &&
-          <>
-            <Link to="/favorites">
-              <h1 className="hover:underline hover:cursor-pointer"> Favorited Games </h1>
-            </Link>
-            <h1 className="hover:underline hover:cursor-pointer" onClick={() => { setNewsFeedShown(prev => !prev) }}> {newsFeedShown == true ? "Hide News Feed" : "Show News Feed"} </h1>
-          </>
-        }
-      </div>
+      }
     </nav>
   )
 }

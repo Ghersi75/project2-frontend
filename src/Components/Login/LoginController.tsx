fdd05ba942f6374f5d511760b2b81c664364e230
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-11 13:27:19 -0500

    Updated to use defaultOptions for request, removed then branch since its not used, updated setError to single line and added a fallback error message

--- src/Components/Login/LoginController.tsx
+++ src/Components/Login/LoginController.tsx
@@ -2,32 +2,24 @@ import { useState } from "react";
 import Login from "./Login";
 import axios from "axios";
 import RedirectIfLoggedIn from "../RouteGuards/RedirectIfLoggedIn";
+import { useDefaultRequestOptions } from "@/Hooks/useDefaultRequestOptions";
 
 export default function LoginController() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const { defaultOptions } = useDefaultRequestOptions();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("")
 
-    axios.post(`${import.meta.env.VITE_BACKEND}/user/login`, {
+    axios.post(`${process.env.VITE_BACKEND}/user/login`, {
       username,
       password
-    }, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true
-    })
-      .then(res => console.log(res))
+    }, defaultOptions)
       .catch(err => {
-        if (err?.data?.error) {
-          setError(err?.data?.error);
-        } else if (err?.message) {
-          setError(err?.message);
-        }
+        setError(err.response?.data?.error || err.message || "Error logging in. Try again later")
       });
   }
 

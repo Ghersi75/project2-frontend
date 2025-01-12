397e4c3231f6efb96d0c2e3f1a108a9fb4be4ef4
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-11 19:09:39 -0500

    Updated home controller to handle fetching data and passing it to home component

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import GameCard from "./GameCard";
 import { HomeGameInfoType } from "@/Types/GameAPIReturnTypes";
 
-export default function Home() {
-  const [specials, setSpecials] = useState<HomeGameInfoType[]>([]);
-  const [topSellers, setTopSellers] = useState<HomeGameInfoType[]>([]);
-  const [newReleases, setNewReleases] = useState<HomeGameInfoType[]>([]);
-  const [comingSoon, setComingSoon] = useState<HomeGameInfoType[]>([]);
-
-  useEffect(() => {
-    axios.get(import.meta.env.VITE_STEAM_FEATURED)
-      .then(res => {
-        setSpecials(res.data.specials.items);
-        setTopSellers(res.data.top_sellers.items);
-        setNewReleases(res.data.new_releases.items);
-        setComingSoon(res.data.coming_soon.items);
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, []);
-
+export default function Home({
+  specials,
+  topSellers,
+  newReleases,
+  comingSoon
+}: {
+  specials: HomeGameInfoType[],
+  topSellers: HomeGameInfoType[],
+  newReleases: HomeGameInfoType[],
+  comingSoon: HomeGameInfoType[]
+}) {
   return (
     <div className="grow p-8 flex flex-col gap-4">
       <h1 className="text-3xl"> Current Specials </h1>
--- src/Components/Home/HomeController.tsx
+++ src/Components/Home/HomeController.tsx
@@ -1,7 +1,31 @@
-import { Home } from "lucide-react";
+import { HomeGameInfoType } from "@/Types/GameAPIReturnTypes";
+import Home from "./Home";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function HomeController() {
+  const [specials, setSpecials] = useState<HomeGameInfoType[]>([]);
+  const [topSellers, setTopSellers] = useState<HomeGameInfoType[]>([]);
+  const [newReleases, setNewReleases] = useState<HomeGameInfoType[]>([]);
+  const [comingSoon, setComingSoon] = useState<HomeGameInfoType[]>([]);
+
+  useEffect(() => {
+    axios.get(import.meta.env.VITE_STEAM_FEATURED)
+      .then(res => {
+        setSpecials(res.data.specials.items);
+        setTopSellers(res.data.top_sellers.items);
+        setNewReleases(res.data.new_releases.items);
+        setComingSoon(res.data.coming_soon.items);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, []);
   return (
-    <Home />
+    <Home
+      specials={specials}
+      topSellers={topSellers}
+      newReleases={newReleases}
+      comingSoon={comingSoon} />
   )
 }

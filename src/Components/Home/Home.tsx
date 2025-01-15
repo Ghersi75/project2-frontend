1a28eb3500fd4cedceb70d1709972d55c717ae92
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-15 10:05:13 -0500

    Made minor styling changes

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -18,18 +18,18 @@ export default function Home({
     <div className="grow p-8 flex flex-col gap-4">
       {
         loading ?
-          <h1 className="text-3xl"> Loading </h1>
+          <h1 className="text-4xl"> Loading </h1>
           :
           specials.length == 0 &&
           topSellers.length == 0 &&
           newReleases.length == 0 &&
           comingSoon.length == 0 &&
-          <h1 className="text-3xl"> Error fetching games, please try reloading the page </h1>
+          <h1 className="text-4xl"> Error fetching games, please try reloading the page </h1>
       }
       {
         specials.length > 0 &&
         <>
-          <h1 className="text-3xl"> Current Specials </h1>
+          <h1 className="text-4xl"> Current Specials </h1>
           <div className="grid gap-8 2xl:grid-cols-3 grid-cols-2 w-full justify-end">
             {
               specials.map((item, idx) => {
@@ -44,7 +44,7 @@ export default function Home({
       {
         topSellers.length > 0 &&
         <>
-          <h1 className="text-2xl"> Top Sellers </h1>
+          <h1 className="text-4xl mt-12"> Top Sellers </h1>
           <div className="grid gap-8 2xl:grid-cols-3 grid-cols-2 w-full justify-end">
             {
               topSellers.map((item, idx) => {
@@ -59,7 +59,7 @@ export default function Home({
       {
         newReleases.length > 0 &&
         <>
-          <h1 className="text-2xl"> New Releases </h1>
+          <h1 className="text-4xl mt-12"> New Releases </h1>
           <div className="grid gap-8 2xl:grid-cols-3 grid-cols-2 w-full justify-end">
             {
               newReleases.map((item, idx) => {
@@ -74,7 +74,7 @@ export default function Home({
       {
         comingSoon.length > 0 &&
         <>
-          <h1 className="text-2xl"> Coming Soon </h1>
+          <h1 className="text-4xl mt-12"> Coming Soon </h1>
           <div className="grid gap-8 2xl:grid-cols-3 grid-cols-2 w-full justify-end">
             {
               comingSoon.map((item, idx) => {
